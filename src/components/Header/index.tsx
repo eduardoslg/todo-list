39aa0a15edb3bbea5dd12171cfcf6f7fb64913cb
eduardoslg <eduardo.seve@hotmail.com>
2023-04-16 12:26:04 -0300

    Finalização do app, criando os componentes task e tasks

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,36 @@
+import { ChangeEvent, FormEvent, useState } from 'react'
 import { AiOutlinePlusCircle } from 'react-icons/ai'
 
 import todoLogo from '../../assets/todoLogo.svg'
 import styles from './header.module.css'
 
-export function Header() {
+interface HeaderProps {
+  onAddTask: (taskTitle: string) => void
+}
+
+export function Header({ onAddTask }: HeaderProps) {
+  const [title, setTitle] = useState('')
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    onAddTask(title)
+    setTitle('')
+  }
+
+  function onChangeTitle(event: ChangeEvent<HTMLInputElement>) {
+    setTitle(event.target.value)
+  }
   return (
     <header className={styles.header}>
       <img src={todoLogo} alt="Logo do app" />
 
-      <form className={styles.newTaskForm}>
-        <input placeholder="Adicione uma nova tarefa" />
+      <form className={styles.newTaskForm} onSubmit={handleSubmit}>
+        <input
+          placeholder="Adicione uma nova tarefa"
+          onChange={onChangeTitle}
+          value={title}
+        />
 
         <button>
           Criar
